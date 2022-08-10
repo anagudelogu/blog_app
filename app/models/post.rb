974690a9e3a8e5dbb90d080c6ceeca72021523cc
Andres Agudelo <anagudelogu@gmail.com>
2022-08-10 15:03:53 -0500

    Implement validations for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { in: 4..250 }
+  validates :commentscounter, comparison: { greater_than_or_equal_to: 0 }
+  validates :likescounter, comparison: { greater_than_or_equal_to: 0 }
+
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :comments
   has_many :likes
 
   def update_post_counter
-    author.postscounter.nil? ? author.update(postscounter: 1) : author.update(postscounter: author.postscounter + 1)
+    author.update(postscounter: author.postscounter + 1)
   end
 
   def most_recent_comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_165848) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_10_195846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_10_165848) do
     t.bigint "user_id", null: false
     t.string "title"
     t.text "text"
-    t.integer "commentscounter"
-    t.integer "likescounter"
+    t.integer "commentscounter", default: 0
+    t.integer "likescounter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
