bbfae0010d6fe215a8829a097ba6176446832312
Andres Agudelo <anagudelogu@gmail.com>
2022-08-10 15:35:27 -0500

    Add specs outline for BDD on Like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have an author'
+  it 'should be associated with a post'
+
+  describe '#update_likes_counter' do
+    it "should add 1 to post's likes counter"
+  end
+
+  describe 'validations' do
+    it 'should not be valid if author is not provided'
+    it 'should not be valid if post is not provided'
+  end
 end
