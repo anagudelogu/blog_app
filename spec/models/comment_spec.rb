42bb40770c8a55df9899a3e435fd104404f7d11f
Andres Agudelo <anagudelogu@gmail.com>
2022-08-10 15:24:42 -0500

    Add specs outline for BDD on Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have an author'
+  it 'should have be associated with a post'
+  it 'should have text'
+
+  describe '#update_comments_counter' do
+    it "should add 1 to post's commentscounter"
+  end
+
+  describe 'Validations' do
+    it 'should not be valid if author is not provided'
+    it 'should not be valid if post is not provided'
+    it 'should not be valid if text is not provided'
+  end
 end
