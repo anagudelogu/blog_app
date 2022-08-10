62a9b413e01916246aa35c8a44c5d95d6780ec9d
Andres Agudelo <anagudelogu@gmail.com>
2022-08-10 11:53:13 -0500

    Create basic specs and outline for BDD validations on User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'name', photo: 'photo', bio: 'bio') }
+  before { user.save }
+
+  it 'should have a name' do
+    expect(user).to have_attributes(name: 'name')
+  end
+
+  it 'should have a photo' do
+    expect(user).to have_attributes(photo: 'photo')
+  end
+
+  it 'should have a bio' do
+    expect(user).to have_attributes(bio: 'bio')
+  end
+
+  describe 'Validations' do
+    it 'should not be valid if a name is not provided'
+
+    describe '@postscounter' do
+      it 'should be an integer'
+      it 'should be greater than or equal than 0'
+    end
+  end
 end
