0130870f64e464a9bc75bb2bb5c7a531a5cfb759
Andres Agudelo <anagudelogu@gmail.com>
2022-08-10 15:38:07 -0500

    Add Like basic implementation specs

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  it 'should have an author'
-  it 'should be associated with a post'
+  let(:author) { User.new(name: 'name', photo: 'photo', bio: 'bio') }
+  let(:post) { Post.new(author:, title: 'title', text: 'text') }
+  let(:like) { Like.new(author:, post:) }
+
+  before do
+    author.save
+    post.save
+    like.save
+  end
+
+  it 'should have an author' do
+    expect(like).to have_attributes(author:)
+  end
+  it 'should be associated with a post' do
+    expect(like).to have_attributes(post:)
+  end
 
   describe '#update_likes_counter' do
     it "should add 1 to post's likes counter"
