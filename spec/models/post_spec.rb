dd823c0f30ccb9280327aa9e0728886ae9c24254
Andres Agudelo <anagudelogu@gmail.com>
2022-08-10 15:03:34 -0500

    Add validations specs for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Post, type: :model do
   let(:author) { User.new(name: 'name', photo: 'photo', bio: 'bio') }
   let(:post) { Post.new(author:, title: 'title', text: 'text') }
 
+  before do
+    author.save
+    post.save
+  end
+
   it 'should have an author' do
     expect(post).to have_attributes(author:)
   end
@@ -26,7 +31,7 @@ RSpec.describe Post, type: :model do
 
   describe '#most_recent_comments' do
     before do
-      (1..10).each { Comment.create(author:, post:, text: 'text') }
+      10.times { Comment.create(author:, post:, text: 'text') }
     end
 
     it 'should return an array of comments' do
@@ -41,17 +46,36 @@ RSpec.describe Post, type: :model do
   end
 
   describe 'Validations' do
-    it 'should not be valid if a title is not provided'
-    it 'should not be valid if the title is exceeds 250 characters'
+    it 'should not be valid if a title is not provided' do
+      post.title = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'should not be valid if the title is exceeds 250 characters' do
+      post.title = 'a' * 251
+      expect(post).not_to be_valid
+    end
 
     describe '@commentscounter' do
-      it 'should be an Integer'
-      it 'should be greater than or equal to zero'
+      it 'should be an Integer' do
+        expect(post.commentscounter).to be_an(Integer)
+      end
+
+      it 'should not be valid if is less than 0' do
+        post.commentscounter = -1
+        expect(post).not_to be_valid
+      end
     end
 
     describe '@likescounter' do
-      it 'should be an Integer'
-      it 'should be greater than or equal to zero'
+      it 'should be an Integer' do
+        expect(post.likescounter).to be_an(Integer)
+      end
+
+      it 'should be greater than or equal to zero' do
+        post.likescounter = -1
+        expect(post).not_to be_valid
+      end
     end
   end
 end
