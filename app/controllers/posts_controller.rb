b8279cb42793da27d8870fafc1bf52e1cea697bb
Andres Agudelo <anagudelogu@gmail.com>
2022-08-16 17:21:07 -0500

    Update Posts#index view and basic styles to follow the wireframe

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @author = User.find(params[:user_id])
+    @posts = Post.where(author: @author).order(created_at: :desc)
+  end
 
   def show
     @post_id = params[:id]
