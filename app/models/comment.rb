45db3e8e7b29fe6f49de6bb75b28878d3a4cc04b
Andres Agudelo <anagudelogu@gmail.com>
2022-08-16 17:04:42 -0500

    Update Post, Comment, and Like models with after_create callback
    After creating one of the above models, is going to increment their specific counters,
    for example, creating a Post, would increment user's postcounter by 1. Creating a
    Comment or Like is going to increment post's commentscounter or likescounter by 1.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
+  after_create :update_comments_counter
+
   def update_comments_counter
     post.update(commentscounter: post.commentscounter + 1)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
+  after_create :update_likes_counter
+
   def update_likes_counter
     post.update(likescounter: post.likescounter + 1)
   end
