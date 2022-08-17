f81ed60beb2614139e311171e104e4423d83c54f
Andres Agudelo <anagudelogu@gmail.com>
2022-08-17 17:12:43 -0500

    Add Post#new action to Postscontroller with @post and @current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,15 @@ class PostsController < ApplicationController
   def index
     @author = User.find(params[:user_id])
     @posts = Post.where(author: @author).order(created_at: :desc)
+    @current_user = current_user
   end
 
   def show
     @post = Post.where(user_id: params[:user_id]).find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+    @current_user = current_user
   end
 end
