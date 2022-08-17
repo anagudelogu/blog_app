6ceda230d32eac938cd7d8efb2992a4b427b5075
Andres Agudelo <anagudelogu@gmail.com>
2022-08-16 19:09:06 -0500

    Update User requests specs since expecting a placeholder now doesn't make sense

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersController', type: :request do
+  let(:user) { create(:user, name: 'Andres') }
+
   describe 'GET /users' do
     before do
       get users_path
@@ -10,29 +12,21 @@ RSpec.describe 'UsersController', type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'should return the correct placeholder text inside the response' do
-      expect(response.body).to include("<h1>here's a list of users</h1>")
-    end
-
-    it 'should render the correct template' do
+    it 'should render the index template' do
       expect(response).to render_template(:index)
     end
   end
 
   describe 'GET /users/:id' do
     before do
-      get user_path(id: 'id')
+      get user_path(id: user.id)
     end
 
     it 'should return an HTTP status 200 (ok)' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'should return the correct placeholder text inside the response' do
-      expect(response.body).to include("<h1>Here's user id </h1>")
-    end
-
-    it 'should render the correct template' do
+    it 'should render the show template' do
       expect(response.body).to render_template(:show)
     end
   end
