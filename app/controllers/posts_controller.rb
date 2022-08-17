e77a04db03e4e04663cfbe68fa6ff63751920cc0
Andres Agudelo <anagudelogu@gmail.com>
2022-08-17 17:16:17 -0500

    Create Posts#create action which handles Post validation logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,24 @@ class PostsController < ApplicationController
     @post = Post.new
     @current_user = current_user
   end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      flash[:notice] = ['Post Succesfully Created!']
+      redirect_to :user_posts
+    else
+      flash[:alert] = post.errors.full_messages
+      redirect_to :new_user_post
+    end
+  end
+
+  private
+
+  def post_params
+    author = User.find(params[:user_id])
+    hash = params.require(:post).permit(:title, :text)
+    hash[:author] = author
+    hash
+  end
 end
