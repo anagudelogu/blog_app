feb3bbdc1a3c72834bdd9bef7a6972014c7a20cb
Andres Agudelo <anagudelogu@gmail.com>
2022-08-18 18:02:40 -0500

    Implement the behavior described by Like#update_likes_counter specs

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,9 +3,10 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
 
-  after_create :update_likes_counter
+  after_create { update_likes_counter(:increment) }
+  after_destroy { update_likes_counter(:decrement) }
 
-  def update_likes_counter
-    post.update(likescounter: post.likescounter + 1)
+  def update_likes_counter(action)
+    action == :increment ? post.increment!(:likescounter) : post.decrement!(:likescounter)
   end
 end
