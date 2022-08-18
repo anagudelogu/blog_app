6b34ee7bca72bc951432261da01d63d5be21c0c1
Andres Agudelo <anagudelogu@gmail.com>
2022-08-18 16:15:56 -0500

    Add index to Likes table to validate uniqueness of Likes at database level, One user can like only once the same post.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_195846) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_211229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_10_195846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
