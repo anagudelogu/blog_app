e9f9c9568c7f0d592072f4373ed5ec1a375d3aec
Andres Agudelo <anagudelogu@gmail.com>
2022-08-22 08:43:05 -0500

    Remove unnecesary queries/variables from Posts controller/views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,14 @@
 class PostsController < ApplicationController
   def index
     @author = User.find(params[:user_id])
-    @posts = Post.where(author: @author).order(created_at: :desc)
   end
 
   def show
-    @post = Post.where(user_id: params[:user_id]).find(params[:id])
-    @current_user = current_user
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
-    @current_user = current_user
   end
 
   def create
@@ -19,7 +16,7 @@ class PostsController < ApplicationController
     if post.save
       redirect_to :user_posts, notice: 'Post Succesfully Created!'
     else
-      redirect_to :new_user_post, alert: post.errors.full_messages.first
+      redirect_to :new_user_post, alert: post.errors.full_messages.to_sentence
     end
   end
 
