250f1b057c0cabaab8bc6f8c250c0de0a28589d4
Andres Agudelo <anagudelogu@gmail.com>
2022-08-23 06:13:42 -0500

    Update users factory to have a real image for system specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user, aliases: [:author] do
     name { 'MyString' }
-    photo { 'MyString' }
+    photo { 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }
     bio { 'MyText' }
   end
 end
