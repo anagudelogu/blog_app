e9f8b7de506a8f761a66e7d243cf221589f6647e
Juan Rosario <jrosario19@hotmail.com>
2022-08-24 11:21:47 -0400

    Redirect user to login page when close session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
   end
+
+  private
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
