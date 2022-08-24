cf078de4cea7dc1d692615ac2c10be04a99fce3c
Andres Agudelo <anagudelogu@gmail.com>
2022-08-24 14:10:12 -0500

    Update User factory to include email and password fields

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :user, aliases: [:author] do
     name { 'MyString' }
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
+    password { 'password' }
     photo { 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }
     bio { 'MyText' }
   end
