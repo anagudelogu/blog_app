e2dcecd56898c5d5de9ae61e3cf8a690b8839d17
Andres Agudelo <anagudelogu@gmail.com>
2022-08-26 12:35:41 -0500

    Fix linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,25 +1,25 @@
 class Api::V1::CommentsController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    load_and_authorize_resource
-  
-    def index
-      @comments = Comment.where(post_id: params[:post_id])
-      render json: @comments
-    end
-  
-    def create
-      @comment.user_id = current_user.id
-  
-      if @comment.save
-        render json: { message: 'Succesfully created', comment: @comment }, status: :created
-      else
-        render json: { message: @comment.errors.full_messages.to_sentence }, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text).merge(post_id: params[:post_id])
+  skip_before_action :verify_authenticity_token
+  load_and_authorize_resource
+
+  def index
+    @comments = Comment.where(post_id: params[:post_id])
+    render json: @comments
+  end
+
+  def create
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      render json: { message: 'Succesfully created', comment: @comment }, status: :created
+    else
+      render json: { message: @comment.errors.full_messages.to_sentence }, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(post_id: params[:post_id])
+  end
+end
